7859e6211e79ced1ae30bf70f9269f6fc6de6c25
rcm264 <rcm264@nau.edu>
2019-10-07 21:15:46 -0700

    Added player controlled gatherer and animation for him

--- project_2/game.js
+++ project_2/game.js
@@ -7,15 +7,13 @@ var renderer = PIXI.autoDetectRenderer({width: WIDTH, height: HEIGHT, background
 gameport.appendChild(renderer.view);
 var stage = new PIXI.Container();
 
-
-
-/// Menu Stage ///
+/// Menu Stage ////////////////////////
 var menuStage = new PIXI.Container();
 stage.addChild(menuStage);
 var menuBackground = new PIXI.Sprite(PIXI.Texture.fromImage("Assets/Backgrounds/background-menu.png"));
 menuStage.addChild(menuBackground);
 
-// Play Button
+// Add play Button
 var playButton = new PIXI.Sprite(PIXI.Texture.fromImage("Assets/Buttons/button-play.png"));
 playButton.anchor.set(0.5);
 playButton.position.set(250,250);
@@ -23,14 +21,16 @@ playButton.interactive = true;
 playButton.buttonMode = true;
 playButton.on('mousedown', playButtonHandler);
 menuStage.addChild(playButton);
+
+// Handles mouse click on play button
 function playButtonHandler(e)
 {
-  stage.removeChild(menuStage);
-  stage.addChild(gameStage);
-  gameLoop();
+  stage.removeChild(menuStage); // leave main menu
+  stage.addChild(gameStage);    // Go to game stage
+  setInterval(spawnApples, 1000);
 }
 
-// Instructions Button
+// Add instructions Button
 var instrButton = new PIXI.Sprite(PIXI.Texture.fromImage("Assets/Buttons/button-instructions.png"));
 instrButton.anchor.set(0.5);
 instrButton.position.set(250,350);
@@ -38,15 +38,17 @@ instrButton.interactive = true;
 instrButton.buttonMode = true;
 instrButton.on('mousedown', instrButtonHandler);
 menuStage.addChild(instrButton);
+
+// Handles mouse click on instructions button
 function instrButtonHandler(e)
 {
-  stage.removeChild(menuStage);
-  stage.addChild(instrStage);
+  stage.removeChild(menuStage); // Leave main menu
+  stage.addChild(instrStage);   // Go to instructions menu
 }
+/// END of Menu stage /////////////////
 
 
-
-/// Instructions Stage ///
+/// Instructions Stage ////////////////
 var instrStage = new PIXI.Container();
 var instrBackground = new PIXI.Sprite(PIXI.Texture.fromImage("Assets/Backgrounds/background-instructions.png"));
 instrStage.addChild(instrBackground);
@@ -57,67 +59,143 @@ returnButton.interactive = true;
 returnButton.buttonMode = true;
 returnButton.on('mousedown', returnButtonHandler);
 instrStage.addChild(returnButton);
+
+// Handles mouse click on return button
 function returnButtonHandler(e)
 {
-  stage.removeChild(instrStage);
-  stage.addChild(menuStage);
+  stage.removeChild(instrStage);  // Leave instructions menu
+  stage.addChild(menuStage);      // Go to main menu
 }
 
+// Instructions text
 var instrText = new PIXI.Text('instructions here');
 instrText.anchor.set(0.5);
 instrText.position.set(250,250);
 instrStage.addChild(instrText);
+/// END of instrucions Stage //////////
 
-
-/// Game Stage ///
+/// Game Stage ////////////////////////
 var gameStage = new PIXI.Container();
 var gameBackground = new PIXI.Sprite(PIXI.Texture.fromImage("Assets/Backgrounds/background-tree.png"));
 gameStage.addChild(gameBackground);
 var appleTexture = new PIXI.Texture.fromImage("Assets/Sprites/apple.png");
-var apple = new PIXI.Sprite(appleTexture);
-apple.vx = 0;
-apple.vy = 0;
-gameStage.addChild(apple);
+/// End of game stage /////////////////
 
-/*var numOfApples = 0;
+/// Game logic ////////////////////////
+// gatherer
+var gatherer;
+
+PIXI.loader
+        .add("Assets/Sprites/gathererSpriteSheet.json")
+        .load(gatherer);
+
+function gatherer()
+{
+  var spriteSheet = PIXI.Loader.shared.resources["Assets/Sprites/gathererSpriteSheet.json"].spritesheet;
+  gatherer = new PIXI.AnimatedSprite(spriteSheet.animations["gatherer"]);
+  gatherer.anchor.set(0.5, 1.0);
+  gatherer.position.set(WIDTH/2, HEIGHT-20);
+  gatherer.animationSpeed = 0.05;
+  gatherer.loop = false;
+  gameStage.addChild(gatherer);
+}
+
+// Gatherer controls
+function gathererKeyDownHander(e)
+{
+  if(e.keyCode == 32)
+  {
+    gatherer.gotoAndPlay(0);
+    gatherer.onComplete(gatherer.gotoAndStop(0));
+    //gatherer.gotoAndStop(0);
+    //gatherer
+  }
+
+  if(e.keyCode == 65) { gatherer.position.x -= 10;} // A
+  if(e.keyCode == 68) { gatherer.position.x += 10; } // D
+
+  if(gatherer.position.x > WIDTH) {gatherer.position.x = 0;}
+  if(gatherer.position.x < 0) {gatherer.position.x = WIDTH;}
+}
+document.addEventListener('keydown', gathererKeyDownHander);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Creates the apples at the top of the screen at a random x position.
+var appleCount = 0;
 function spawnApples()
 {
-  if(numOfApples < 5)
+  if(appleCount < 3)
   {
     var apple = new PIXI.Sprite(appleTexture);
     apple.anchor.set(0.5);
     apple.x = Math.floor(Math.random() * WIDTH);
     apple.y = 15;
     gameStage.addChild(apple);
-    numOfApples += 1;
+    appleCount++;
+    appleFall(apple);
   }
 }
-spawnApples();*/
 
-
-function gameLoop()
+/*function gameLoop()
 {
-  requestAnimationFrame(gameLoop);
+  var apple = spawnApples();
+  new_x = 250;
+  new_y = HEIGHT;
+  createjs.Tween.get(apple.position).to({x: new_x, y: new_y}, 1000);
+}*/
 
-  apple.vx = 1;
-  apple.vy = 1;
+// Drops the apples toward the grown from their spawn location.
+function appleFall(apple)
+{
+  setTimeout(function()
+  {
+    /*if(apple.position.y >= gatherer.position.y-32 &&
+       apple.position.y <= gatherer.position.y+32 &&
+       apple.position.x-8 <= gatherer.position.x+32 &&
+       apple.position.x-8 >= gatherer.position.x-32)
+       {
+         gameStage.removeChild(apple);
+         appleCount -= 1;
+         return;
+       }*/
+    if(apple.y <= HEIGHT + 15)
+    {
+      gameStage.removeChild(apple);
+      appleCount -= 1;
+      return;
+    }
 
-  apple.x += apple.vx;
-  apple.y += apple.vy;
-
-  renderer.render(stage);
+    requestAnimationFrame(function(temp)
+    {
+      appleFall(apple);
+    });
+    apple.y += 5;
+  }, 1000/60);
 }
-gameLoop();
-
-
-
-/*
+/// END of game logic /////////////////
 
 function animate()
 {
     requestAnimationFrame(animate);
-    //apple.y += 10;
     renderer.render(stage);
 }
 animate();
-*/
